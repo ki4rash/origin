fe1ec3e155a98ef665fd685a99e7553b67603236
ki4rash <kiarash.shadloo@gmail.com>
2019-08-08 19:31:33 +0430

    final22

---
+++ app/src/main/java/com/illuminati/origin/illuminati4.java
@@ -0,0 +1,14 @@
+package com.illuminati.origin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class illuminati4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_illuminati4);
+    }
+}
