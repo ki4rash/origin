fd243864d485d1a5f7e4b6825f331a84cfafd4ff
ki4rash <kiarash.shadloo@gmail.com>
2019-08-09 01:51:35 +0430

    kia3

--- app/src/main/java/com/illuminati/origin/illuminati2.java
+++ app/src/main/java/com/illuminati/origin/illuminati2.java
@@ -3,6 +3,8 @@ package com.illuminati.origin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class illuminati2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class illuminati2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_illuminati2);
+
+        ImageButton btnshow=findViewById(R.id.btncall);
+        btnshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
