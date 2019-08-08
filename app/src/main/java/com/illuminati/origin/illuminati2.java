a1527db533be5c67e16fd1d1ebbdf898191f84f6
ki4rash <kiarash.shadloo@gmail.com>
2019-08-08 13:25:29 +0430

    illuminati

---
+++ app/src/main/java/com/illuminati/origin/illuminati2.java
@@ -0,0 +1,14 @@
+package com.illuminati.origin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class illuminati2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_illuminati2);
+    }
+}
