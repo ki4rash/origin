797fb874ca0af187576f0dbb5b1d6c499d516e0e
ki4rash <kiarash.shadloo@gmail.com>
2019-08-09 17:14:59 +0430

    kia4

--- app/src/main/java/com/illuminati/origin/MainActivity.java
+++ app/src/main/java/com/illuminati/origin/MainActivity.java
@@ -2,13 +2,45 @@ package com.illuminati.origin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnVerifyPhoneNumber = (Button) findViewById(R.id.btnDial);
+
+        btnVerifyPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button btnProfile = (Button) findViewById(R.id.btnProfile);
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
+
     }
+
+
 }
