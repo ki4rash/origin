ab6708224bc5ef4ef33ffe3334be11a4a451bb43
ki4rash <kiarash.shadloo@gmail.com>
2019-08-09 16:53:01 +0430

    neweone

--- app/src/main/java/com/illuminati/origin/illuminati2.java
+++ app/src/main/java/com/illuminati/origin/illuminati2.java
@@ -2,23 +2,35 @@ package com.illuminati.origin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class illuminati2 extends AppCompatActivity {
+
+    private EditText editText_num;
+    private ImageButton imageButton_dialer;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_illuminati2);
-
-        ImageButton btnshow=findViewById(R.id.btncall);
-        btnshow.setOnClickListener(new View.OnClickListener() {
+        editText_num =(EditText) findViewById(R.id.edtCall);
+        imageButton_dialer  = (ImageButton) findViewById(R.id.btncall);
+        imageButton_dialer . setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData (Uri.parse("tel:" + editText_num.getText().toString() ));
+                startActivity(intent);
             }
         });
+
+
     }
 }
