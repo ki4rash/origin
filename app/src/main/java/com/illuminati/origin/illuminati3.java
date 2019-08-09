fd8171c0809228139a3709386641c14db3e1166a
ki4rash <kiarash.shadloo@gmail.com>
2019-08-09 21:57:03 +0430

    kia666

--- app/src/main/java/com/illuminati/origin/illuminati3.java
+++ app/src/main/java/com/illuminati/origin/illuminati3.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class illuminati3 extends AppCompatActivity {
 
@@ -17,11 +18,23 @@ public class illuminati3 extends AppCompatActivity {
         setContentView(R.layout.activity_illuminati3);
         Button btnVerifyPhoneNumber = (Button) findViewById(R.id.BtnReview);
 
+
+        final EditText EdtFname = (EditText)  findViewById(R.id.EdtTagName);
+        final EditText EdtFamily = (EditText)  findViewById(R.id.EdtTagFamily);
+        final EditText EdtAge = (EditText)  findViewById(R.id.EtdTagage);
+        final EditText Edtphnumber = (EditText)  findViewById(R.id.EdtTagPhonenumber);
+        final EditText EdtAddress = (EditText)  findViewById(R.id.EdtTagAddress);
+
         btnVerifyPhoneNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(illuminati3.this, illuminati4.class);
-                startActivity(i);
+                String name1 = EdtFname.getText().toString();
+                String name2 = EdtFamily.getText().toString();
+                String name3 = EdtAge.getText().toString();
+                String name4 = Edtphnumber.getText().toString();
+                String name5 = EdtAddress.getText().toString();
+                Intent intent = new Intent(illuminati3.this, illuminati4.class);
+                startActivityForResult(intent, 180);
             }
         });
     }
